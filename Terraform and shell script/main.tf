4ed5dc3d069290a1b984bae23cdcf2f69019f438
Amith P <107483173+amithpalissery@users.noreply.github.com>
2025-08-13 23:25:27 -0700

    Update main.tf

--- Terraform and shell script/main.tf
+++ Terraform and shell script/main.tf
@@ -1,1 +1,12 @@
-.
+resource "aws_instance" "web" {
+  ami           = "ami-04f59c565deeb2199"
+  instance_type = "t2.medium"
+  key_name      = "amithnv"
+  # No security group specified = uses default
+  
+  user_data = file("${path.module}/setup_k8s.sh")
+  
+  tags = {
+    Name = "Amith_Instance1"
+  }
+}
